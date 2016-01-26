555ad99f28bf96339933d0e9b1006ffb566ff3ab
John Burrin <john.burrin@thesalegroup.co.uk>
2016-01-26 12:56:07 +0000

    Much refatcoring and documentation-ing

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,4 @@
 /*
-  Receives from the hardware serial, sends to software serial.
-  Receives from software serial, sends to hardware serial.
-
-  The circuit:
-   RX is digital pin 14 (connect to TX of other device)
-   TX is digital pin 12 (connect to RX of other device)
 
 */
 
@@ -12,21 +6,19 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>   
 #include <SoftwareSerial.h> 
-#include "DS1302.h"
-#include "EpochTime.h"
+#include <DS1302.h>
+#include <EpochTime.h>
 
 // Init the DS1302
-DS1302 rtc(2, 13,15);
+DS1302 rtc(CEpin, IOpin, SCLKpin);
+
 // Init a Time-data structure
 Time t;
-
 
 String getClientOutput(void);
 String getcsvOutput(void);
 
-SoftwareSerial mySerial(14, 12); // RX, TX
-int green_led = 5;
-int red_led = 4;
+SoftwareSerial mySerial(SSRX, SSTX); // RX, TX
 
 const char* ssid = SSSID;
 const char* password = PASSWORD;
@@ -38,18 +30,19 @@ double windSpeed = 0.0;
 int humidity = 0;
 int lightValue = 0;
 unsigned long int rainTipperCounter = 0;
-unsigned long int lastUpdate = 0;
 
 String s = "";
 
-WiFiServer server(80);
+// Create an instance of the server
+// specify the port to listen on as an argument
+WiFiServer server(WEB_PORT);
 
 byte dataByte;
 
 byte buffer[24];
 int bufferindex = 0;
 
-// I'v define a packet to be as follows
+// I've define a packet to be as follows
 // H is header   3 chars
 // D is data     12 chars
 // C is checksum 2 chars
@@ -67,31 +60,29 @@ int bufferindex = 0;
 // byte 10 Wind speed high byte
 // byte 11 Wind speed low byte
 struct packet {
-  byte header[3];
-  byte data[14];
-  byte checksum[2];
+  byte header[HEADER_SIZE];
+  byte data[DATA_SIZE];
+  byte checksum[CHECKSUM_SIZE];
 }my_packet;
 
 void setup()
 {
-    // Set the clock to run-mode, and disable the write protection
+  #ifdef DEBUG
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  Serial.println("Beginning setup");
+#endif
+
+  // Set the clock to run-mode, and disable the write protection
   rtc.halt(false);
   rtc.writeProtect(true);
 
-  pinMode(green_led, OUTPUT);
-  pinMode(red_led, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
 
-#ifdef DEBUG
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  Serial.println("Goodnight moon!");
-#endif
   // set the data rate for the SoftwareSerial port
   mySerial.begin(9600);
-  // Create an instance of the server
-  // specify the port to listen on as an argument
-
-
+  
   // Connect to WiFi network
 #ifdef DEBUG
   Serial.println();
@@ -102,14 +93,14 @@ void setup()
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(red_led, HIGH);
-    digitalWrite(green_led, HIGH);
+    digitalWrite(RED_LED, HIGH);
+    digitalWrite(GREEN_LED, HIGH);
     delay(500);
 #ifdef DEBUG
     Serial.print(".");
 #endif
-    digitalWrite(red_led, LOW);
-    digitalWrite(green_led, LOW);
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(GREEN_LED, LOW);
   }
 #ifdef DEBUG
   Serial.println("");
@@ -126,13 +117,19 @@ void setup()
 #ifdef DEBUG
   Serial.println(WiFi.localIP());
 #endif
-  digitalWrite(red_led, LOW);
-  digitalWrite(green_led, LOW);
+
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
+
+  #ifdef DEBUG
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  Serial.println("Setup complete");
+#endif
 }
 
-void loop() // run over and over
+void loop()
 {
-  lastUpdate = millis() / 1000;
   if (mySerial.available()) {
     dataByte = mySerial.read();
 
@@ -161,16 +158,14 @@ void loop() // run over and over
 
       /// Perform checksum
       int checksum = 0;
-      for (int i = 0; i < 14; ++i) {
+      for (int i = 0; i < DATA_SIZE; ++i) {
         checksum += my_packet.data[i];
       }
+
       if (checksum == ( my_packet.checksum[0] << 8 ) + my_packet.checksum[1]) {
-        digitalWrite(green_led, HIGH);
-       // Serial.print(getEpochTime(rtc.getTime()));
-  Serial.println();
-#ifdef DEBUG
-        Serial.println("Checksum valid");
-#endif
+        digitalWrite(GREEN_LED, HIGH);
+
+        // Temperature
         temperature = ( my_packet.data[0] << 8 ) + my_packet.data[1];
         temperature = (temperature / 10) - 120;
 #ifdef DEBUG
@@ -178,6 +173,7 @@ void loop() // run over and over
         Serial.println(temperature);
 #endif
 
+        //Pressure
         pressure = ( my_packet.data[2] << 8 ) + my_packet.data[3];
         pressure = (pressure / 10) - 800;
 #ifdef DEBUG
@@ -192,7 +188,7 @@ void loop() // run over and over
         Serial.print("Wind Dir: = ");
         Serial.println(windDir);
 #endif
-
+        // Humidity
         humidity = my_packet.data[5];
 #ifdef DEBUG
         Serial.print("Humidity = ");
@@ -200,7 +196,7 @@ void loop() // run over and over
         Serial.println("%");
 #endif
 
-
+        // Wind Speed
         windSpeed = ( my_packet.data[10] << 8 ) + my_packet.data[11];
         windSpeed = (windSpeed / 100) - 45;
 #ifdef DEBUG
@@ -210,29 +206,27 @@ void loop() // run over and over
 
 
         // Rain tipper counter
-        // unsigned long int rainTipperCounter;
         rainTipperCounter = my_packet.data[6];
         rainTipperCounter = (rainTipperCounter << 8) + my_packet.data[7];
         rainTipperCounter = (rainTipperCounter << 8) + my_packet.data[8];
         rainTipperCounter = (rainTipperCounter << 8) + my_packet.data[9];
-
 #ifdef DEBUG
         Serial.print("Rain Tipper Counter = ");
         Serial.println(rainTipperCounter );
         
 #endif
 
+        // Light Value
         lightValue = ( my_packet.data[12] << 8 ) + my_packet.data[13];
-
 #ifdef DEBUG
         Serial.print("Light Value = ");
         Serial.println(lightValue);
-        Serial.print(lastUpdate);
+        Serial.print(getEpochTime());
 #endif
       }
 
       bufferindex = 0;
-      digitalWrite(green_led, LOW);
+      digitalWrite(GREEN_LED, LOW);
 #ifdef DEBUG
       Serial.println();
 #endif
@@ -254,7 +248,7 @@ void loop() // run over and over
     while (client.connected()) {
       if (client.available()) {
 #ifdef DEBUG
-        Serial.println("client is available");
+        Serial.println("Client is available");
 #endif
         // Read the first line of the request
         String req = client.readStringUntil('\r');
@@ -263,7 +257,7 @@ void loop() // run over and over
 #endif
         client.flush();
         // Prepare the response
-        digitalWrite(red_led, HIGH);
+        digitalWrite(RED_LED, HIGH);
         // Match the request
         if (req.indexOf("/csv") != -1) {
           s = getcsvOutput();
@@ -276,7 +270,7 @@ void loop() // run over and over
 #ifdef DEBUG
         Serial.println("Client disconnected");
 #endif
-        digitalWrite(red_led, LOW);
+        digitalWrite(RED_LED, LOW);
       }
     }
   }
